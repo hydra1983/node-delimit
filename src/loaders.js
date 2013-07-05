3329b777599b9f0879d4f8aa604d677cfb979747
Trevor Senior <trevor@urban4m.com>
2013-07-05 11:47:12 -0400

    Stripped all non-printable characters - still buggy in terms of replacing smart quotes

--- src/loaders.js
+++ src/loaders.js
@@ -2,7 +2,21 @@ exports.getTsvLoader = function() {
 	var loader = {};
 
 	loader.toDataRow = function(rowString) {
-		return rowString.replace(/\r/g, '').split("\t");
+
+		// convert fancy quotes into normal ones
+		rowString = rowString.replace(/\u2018/g, "'");
+		rowString = rowString.replace(/\u2019/g, "'");
+		rowString = rowString.replace(/\u201c/g, '"');
+		rowString = rowString.replace(/\u201d/g, '"');
+
+		// get rid of any other non printable character
+		rowString = rowString.replace(/[^\x00-\x7F]/g, "");
+
+		// other stuff
+		rowString = rowString.replace(/\r/g, '');
+
+		// split and return
+		return rowString.split("\t");
 	};
 
     return loader;
