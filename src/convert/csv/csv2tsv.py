87ed88ab8297044e4fd2a10452516e6a7268c606
Trevor Senior <trevor@urban4m.com>
2013-08-26 17:18:27 -0400

    specified backslash as an escape character...

--- src/convert/csv/csv2tsv.py
+++ src/convert/csv/csv2tsv.py
@@ -7,7 +7,7 @@ import csv
 tfile_handle, tfile_path = tempfile.mkstemp(suffix='.tsv')
 
 with open(sys.argv[1], 'rb') as csvfile:
-    reader = csv.reader(csvfile)
+    reader = csv.reader(csvfile, quotechar='"', escapechar="\\")
     with open(tfile_path, 'wb') as tsvfile:
         writer = csv.writer(tsvfile, delimiter="\t")
         for row in reader:
@@ -17,6 +17,7 @@ with open(sys.argv[1], 'rb') as csvfile:
             	fixed = fixed.replace('\t', ' ')  # replace tabs with spaces
             	fixed = unicode(fixed, errors='ignore')
                 fixedRow.append(fixed)
+                print fixed
             writer.writerow(fixedRow)
 
 sys.stdout.write(tfile_path)
